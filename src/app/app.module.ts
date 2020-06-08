82495087929c85c15ad6c977dd5881abe027e02a
ManhTTFU <tuanmanh97x@gmail.com>
2020-06-08 10:16:44 +0700

    adđ bootstrap and build project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { ShoppingCardComponent } from './components/shopping-card/shopping-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+    ShoppingCardComponent
   ],
   imports: [
     BrowserModule
